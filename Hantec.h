c5b3c879d7c2e88fa3e9627a15190888b00429d2
NakejTypekZMoravy <77998948+NakejTypekZMoravy@users.noreply.github.com>
2021-01-26 00:16:15 +0100

    Create Hantec.h

---
+++ Hantec.h
@@ -0,0 +1,48 @@
+//Chce to spóstu práce tak makéme než zócna zapadne za Prigl
+//vylepšujte jak chcete...
+
+#pragma once
+#define pro for
+#define dokavať while
+#define pokud if
+#define nebo pokud else if
+#define jinak else
+#define rožínač switch
+#define celé int
+#define plavócá float
+#define písmeno char
+#define dvojité double
+#define krátké short
+#define zlom break
+#define nebo or
+#define taky and
+#define ne not
+#define jen xor
+#define třída class
+#define soukromé private
+#define veřejné public
+#define chráněné protected
+#define toto this
+#define kemoš friend
+#define pravda true
+#define lež false
+#define struktura struct
+#define případ case
+#define lapni catch
+#define přeskoč continue
+#define jdina goto
+#define nové new
+#define konst const
+#define výchozé default
+#define zkus try
+#define bezvymečné noexcept
+#define vracé return
+#define statické static
+#define velikost sizeof
+#define bezmínusa unsigned
+#define smínusem signed
+#define použití using
+#define prázdno void
+#define virtualné virtual
+#define unie union
+#define kompliment compl
